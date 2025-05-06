aa1efdabb918cbb308059aa25efa5ae8366ad70a
khurshid2407 <khurshidalam2407@gmail.com>
2025-05-06 15:45:23 +0530

    Added new test

--- TekPyramid/src/test/java/Organization/CreateOrganizationTest.java
+++ TekPyramid/src/test/java/Organization/CreateOrganizationTest.java
@@ -43,4 +43,10 @@ public class CreateOrganizationTest extends BaseClass{
 		sa.assertAll();	
 	}
 	
+	@Test
+	public void sample()
+	{
+		System.out.println("Jenkin execution will start whenever new build come");
+	}
+	
 }
